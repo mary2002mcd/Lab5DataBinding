9578f781025774cbcdd01d376d3385cb78f86e10
unknown <G00401878@atu.ie>
2023-03-03 12:32:25 +0000

    Lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count:number = 0;
+  hide:boolean = true;
+  
+
+  onClick(){
+    this.count++;
+  }
+
+  onDblClick(){
+    if(this.hide == true)
+    {
+        this.hide = false;
+    }
+    else{
+      this.hide = true;
+    }
+    
+  }
+
+  
 }
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  presidents:string[] = ["Higgins","McAleese", "Robinson", "Hillery"];
+  presidents2: any[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}];
+
+}
